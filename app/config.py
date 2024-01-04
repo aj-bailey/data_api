8a963bc7c989e18bb2ce5c5c9327ae40a4e27089
Adam Bailey <ab67319@gmail.com>
2024-01-04 11:23:45 -0700

    update config

--- app/config.py
+++ app/config.py
@@ -1,9 +1,18 @@
-from sqlalchemy import create_engine
+from environs import Env
+from sqlalchemy import create_engine, URL
 from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
 
-DATABASE_URL = "postgresql://postgresql:postgres@localhost:5432/dht_db"
+env = Env()
+env.read_env(".env")
+
+DATABASE_URL = URL.create(
+        drivername="postgresql+psycopg2",
+        username=env.str("POSTGRES_USER"),
+        password=env.str("POSTGRES_PASSWORD"),
+        host=env.str("DATABASE_HOST"),
+        port=env.str("PORT"),
+        database=env.str("POSTGRES_DB"),
+    ).render_as_string(hide_password=False)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autflush=False, bind=engine)
-Base = declarative_base()
