1a97d9e51c6b1407cebd95928024976227dcfa24
Adam Bailey <ab67319@gmail.com>
2024-01-04 09:44:15 -0700

    create model

---
+++ app/model.py
@@ -0,0 +1,33 @@
+from datetime import datetime
+from typing_extensions import Annotated
+
+from sqlalchemy import BIGINT, FLOAT, func, Integer, TIMESTAMP
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, declared_attr
+
+INT_PK = Annotated[
+    int,
+    mapped_column(
+        Integer, primary_key=True
+    ) 
+]
+
+class Base(DeclarativeBase):
+    pass
+
+class TimestampMixin:
+    created_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        TIMESTAMP, server_default=func.now(), onupdate=func.now()
+    )
+
+class TableNameMixin:
+    @declared_attr.directive
+    def __tablename__(cls) -> str:
+        return cls.__name__.lower() + "s"
+
+class DHTReading(Base, TableNameMixin, TimestampMixin):
+    id: Mapped[int] = mapped_column(BIGINT, primary_key=True)
+    humidity: Mapped[float] = mapped_column(FLOAT)
+    temperature: Mapped[float] = mapped_column(FLOAT)
