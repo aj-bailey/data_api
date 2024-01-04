8a466852b3e4eb8bc379bcd7c8bf86ab3fc5bf6c
Adam Bailey <ab67319@gmail.com>
2024-01-04 11:24:20 -0700

    add application lifespan for creating db

--- app/main.py
+++ app/main.py
@@ -1,7 +1,25 @@
-from fastapi import FastAPI
+from app.model import Base
 
-app = FastAPI()
+from app.config import engine
+from contextlib import asynccontextmanager
+from fastapi import FastAPI
+from sqlalchemy_utils import database_exists, create_database
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    if database_exists(engine.url):
+        print("Database exists!")
+    else:
+        print("Creating database...")
+        create_database(engine.url)
+        print("Database created!")
+        
+    Base.metadata.create_all(bind=engine)
+    yield
+
+app = FastAPI(lifespan=lifespan)
 
 @app.get('/')
 async def Home():
-    return "Welcome Home"
+    print(database_exists(engine.url))
+    return database_exists(engine.url)
