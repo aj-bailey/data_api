0c100d1dea9a96bb02e69a507900eb39fcda6d4b
Adam Bailey <ab67319@gmail.com>
2024-01-04 11:49:48 -0700

    update main

--- app/main.py
+++ app/main.py
@@ -1,25 +1,17 @@
+from app.config import create_db, engine
 from app.model import Base
-
-from app.config import engine
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
-from sqlalchemy_utils import database_exists, create_database
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    if database_exists(engine.url):
-        print("Database exists!")
-    else:
-        print("Creating database...")
-        create_database(engine.url)
-        print("Database created!")
-        
+    create_db()
     Base.metadata.create_all(bind=engine)
     yield
 
 app = FastAPI(lifespan=lifespan)
 
 @app.get('/')
-async def Home():
-    print(database_exists(engine.url))
-    return database_exists(engine.url)
+async def root():
+    return "Home"
