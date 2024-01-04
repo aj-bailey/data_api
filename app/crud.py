b2ff85230a97f25b4b29f9197f708f668b008554
Adam Bailey <ab67319@gmail.com>
2024-01-04 11:56:14 -0700

    add create dht reading crud

---
+++ app/crud.py
@@ -0,0 +1,10 @@
+from app.model import DHTReading
+from app.schema import DHTReadingSchema
+from sqlalchemy.orm import Session
+
+def create_dht_reading(db: Session, dht_reading: DHTReadingSchema):
+    _dht_reading = DHTReading(humidity=dht_reading.humidity, temperature=dht_reading.temperature)
+    db.add(_dht_reading)
+    db.commit()
+    db.refresh(_dht_reading)
+    return _dht_reading
