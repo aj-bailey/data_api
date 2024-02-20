ce253894b847482111d69ea2004615cddf48753d
Adam Bailey <ab67319@gmail.com>
2024-02-19 18:27:41 -0700

    add csv script

---
+++ run_prev_date_to_csv.sh
@@ -0,0 +1,2 @@
+source /home/adam.bailey/.pyenv/versions/data_api/bin/activate
+/home/adam.bailey/.pyenv/versions/data_api/bin/python /home/adam.bailey/Desktop/apps/data_api/scripts/prev_date_to_csv.py
