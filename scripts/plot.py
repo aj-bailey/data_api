6fb1d6d9e525e66270be2b7c3b7ebc702fbe8f33
Adam Bailey <ab67319@gmail.com>
2024-02-20 21:00:42 -0700

    Refactor: Use env variables

--- scripts/plot.py
+++ scripts/plot.py
@@ -1,37 +1,30 @@
+from environs import Env
 import psycopg2
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Database connection parameters
+env = Env()
+env.read_env(".env")
+
 db_params = {
-    "host": "localhost",         # Replace with your database host
-    "database": "dht", # Replace with your database name
-    "user": "postgres",         # Replace with your database user
-    "password": "postgres"  # Replace with your database password
+    "host": env.str("DATABASE_HOST"),
+    "database": env.str("POSTGRES_DB"),
+    "user": env.str("POSTGRES_USER"),
+    "password": env.str("POSTGRES_PASSWORD")
 }
 
-# SQL query to retrieve humidity, temperature, and timesta
 query = "SELECT created_at, humidity, temperature FROM dhtreadings"
 
 try:
-    # Connect to the PostgreSQL database
     conn = psycopg2.connect(**db_params)
-    
-    # Create a cursor object to execute SQL queries
     cursor = conn.cursor()
-
-    # Execute the SQL query
     cursor.execute(query)
-
-    # Fetch all the rows from the result set and load into a Pandas DataFrame
     data = cursor.fetchall()
+    
     columns = ["created_at", "humidity", "temperature"]
     df = pd.DataFrame(data, columns=columns)
-
-    # Convert the "timestamp" column to a datetime object
     df["created_at"] = pd.to_datetime(df["created_at"])
 
-    # df.to_csv("temp_hum_data.csv")
     # Plot for Humidity
     plt.subplot(2, 1, 1)
     plt.plot(df["created_at"], df["humidity"], label="Humidity", linestyle='-', color='green')
@@ -55,8 +48,7 @@ try:
 
     # Adjust subplot spacing
     plt.tight_layout()
-
-    # Show the plots
+    
     plt.show()
 except (Exception, psycopg2.Error) as error:
     print("Error:", error)
