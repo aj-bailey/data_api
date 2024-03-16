a50b6da4becee01e1ad2dbb15e30b3600e3e9943
Adam Bailey <ab67319@gmail.com>
2024-03-16 16:18:48 -0600

    Add cron job for upload script

--- app/main.py
+++ app/main.py
@@ -1,17 +1,24 @@
 from contextlib import asynccontextmanager
 
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from apscheduler.triggers.cron import CronTrigger
 from fastapi import FastAPI
 
 from app.config import create_db, engine
 from app.model import Base
 from app.router import router
+from scripts.upload_csv_data import upload_prev_date_data
 
+scheduler = AsyncIOScheduler()
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     print("creating DB")
     create_db()
     Base.metadata.create_all(bind=engine)
+    
+    scheduler.add_job(upload_prev_date_data, CronTrigger(hour=16, minute=15))
+    scheduler.start()
     yield
 
 app = FastAPI(lifespan=lifespan)
--- scripts/upload_csv_data.py
+++ scripts/upload_csv_data.py
@@ -3,18 +3,19 @@ from datetime import datetime, timedelta
 
 import boto3
 
-today = datetime.now()
-today_str = today.strftime('%Y-%m-%d')
-yesterday = today - timedelta(days=1)
-yesterday_str = yesterday.strftime('%Y-%m-%d')
+def upload_prev_date_data():
+    today = datetime.now()
+    today_str = today.strftime('%Y-%m-%d')
+    yesterday = today - timedelta(days=1)
+    yesterday_str = yesterday.strftime('%Y-%m-%d')
 
-script_dir = os.path.dirname(os.path.abspath(__file__))
-csv_file_path = os.path.join(script_dir, f'{yesterday_str}_data.csv')
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    csv_file_path = os.path.join(script_dir, f'{yesterday_str}_data.csv')
 
-s3 = boto3.client('s3')
-bucket_name = 'iot-daily-values'
-s3_file_name = f'{yesterday.year}/{yesterday.month}/{yesterday_str}_data.csv'
+    s3 = boto3.client('s3')
+    bucket_name = 'iot-daily-values'
+    s3_file_name = f'{yesterday.year}/{yesterday.month}/{yesterday_str}_data.csv'
 
-s3.upload_file(csv_file_path, bucket_name, s3_file_name)
+    s3.upload_file(csv_file_path, bucket_name, s3_file_name)
 
-print(f"File uploaded successfully to {bucket_name}/{s3_file_name}")
+    print(f"File uploaded successfully to {bucket_name}/{s3_file_name}")
