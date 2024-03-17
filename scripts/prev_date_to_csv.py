b36efa7636652376d849facafda21830f7702c2e
Adam Bailey <ab67319@gmail.com>
2024-03-16 18:24:38 -0600

    Refactor prev date to csv script

--- scripts/prev_date_to_csv.py
+++ scripts/prev_date_to_csv.py
@@ -1,36 +1,17 @@
 from datetime import datetime, timedelta
 
 import pandas as pd
-import psycopg2
-from environs import Env
+from sqlalchemy import func, select
 
-env = Env()
-env.read_env(".env")
+from app.config import SessionLocal
+from app.model import DHTReading
 
-db_params = {
-    "host": env.str("DATABASE_HOST"),
-    "database": env.str("POSTGRES_DB"),
-    "user": env.str("POSTGRES_USER"),
-    "password": env.str("POSTGRES_PASSWORD")
-}
 
 def prev_date_to_csv():
     today = datetime.now()
-    today_str = today.strftime('%Y-%m-%d')
     yesterday = today - timedelta(days=1)
-    yesterday_str = yesterday.strftime('%Y-%m-%d')
 
-    query = f"SELECT * FROM dhtreadings WHERE created_at::date = '{yesterday_str}'"
+    stmt = select(DHTReading).where(func.date(DHTReading.created_at) == str(yesterday.date()))
 
-    try:
-        conn = psycopg2.connect(**db_params)
-        df = pd.read_sql_query(query, conn, index_col='id')
-        df.to_csv(f"/home/adam.bailey/Desktop/apps/data_api/scripts/{yesterday_str}_data.csv")
-        print(f"Saved {yesterday_str} data to csv")
-
-    except (Exception, psycopg2.Error) as error:
-        print("Error:", error)
-
-    finally:
-        if conn:
-            conn.close()
+    df = pd.read_sql(stmt, SessionLocal().bind)
+    df.to_csv(f"/home/adam.bailey/Desktop/apps/data_api/scripts/{yesterday.date()}_data.csv")
