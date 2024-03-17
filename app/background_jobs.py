6a848a7e62d501c087b0ddda247576694b3cb71e
Adam Bailey <ab67319@gmail.com>
2024-03-16 18:32:39 -0600

    Update cron times

--- app/background_jobs.py
+++ app/background_jobs.py
@@ -11,7 +11,7 @@ def schedule_jobs():
     scheduler.add_job(prev_date_to_csv, CronTrigger(hour=0, minute=15))
     print("Added prev_date_to_csv to run daily at 00:15")
 
-    scheduler.add_job(upload_prev_date_data, CronTrigger(hour=0, minute=20))
+    scheduler.add_job(upload_prev_date_data, CronTrigger(hour=0, minute=30))
     print("Added upload_prev_date_data to run daily at 00:30")
     
     scheduler.start()
